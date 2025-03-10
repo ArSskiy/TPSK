322f381d33e7e4a3bbe838add13d83ade0d9667f
Артём Краевский <apahaka05@gmail.com>
2025-03-10 14:55:37 +0300

    Добавьте файлы проекта.

---
+++ StudentWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace TPSK
+{
+    /// <summary>
+    /// Логика взаимодействия для StudentWindow.xaml
+    /// </summary>
+    public partial class StudentWindow : Window
+    {
+        public StudentWindow()
+        {
+            InitializeComponent();
+            GroupCmb.SelectedValuePath = "ID";
+            GroupCmb.DisplayMemberPath = "Name";
+            GroupCmb.ItemsSource = App.Ent.Group.ToList();
+
+            StudentCmb.SelectedValuePath = "ID";
+            StudentCmb.DisplayMemberPath = "Name";
+            StudentCmb.ItemsSource = App.Ent.Student.ToList();
+
+            TestCmb.SelectedValuePath = "ID";
+            TestCmb.DisplayMemberPath = "Name";
+            TestCmb.ItemsSource = App.Ent.Test.ToList();
+        }
+
+        private void GroupCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectGroup = Convert.ToInt32(GroupCmb.SelectedValue);
+            StudentCmb.ItemsSource = App.Ent.Student.Where(x => x.IdGroup == selectGroup).ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TestCmb.Text=="Общие основы функционирования субъектов хозяйствования")
+            {
+                Test1Window test1Window = new Test1Window();
+                test1Window.Show();
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Test1Window.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TPSK
+{
+    /// <summary>
+    /// Логика взаимодействия для Test1Window.xaml
+    /// </summary>
+    public partial class Test1Window : Window
+    {
+        int b = 0;
+        int c = 0;
+        public Test1Window()
+        {
+            InitializeComponent();
+        }
+
+        private void p1_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox1.Text=="5")
+            {
+                MessageBox.Show("Правильно");
+                b=b+1;
+            }
+            else MessageBox.Show("Неправильно");
+
+                p1.Visibility = Visibility.Collapsed;
+                products.SelectedItem = v2;
+            
+        }
+
+        private void p2_Click(object sender, RoutedEventArgs e)
+        {
+            if (RB2.IsChecked==true)
+            {
+                MessageBox.Show("Правильно");
+                b = b + 1;
+            }
+            else MessageBox.Show("Неправильно");
+
+            p2.Visibility = Visibility.Collapsed;
+            products.SelectedItem = v3;
+        }
+
+        private void p3_Click(object sender, RoutedEventArgs e)
+        {
+            if (ChB1.IsChecked == false && ChB2.IsChecked == false && ChB3.IsChecked == false && ChB4.IsChecked == false && ChB5.IsChecked == true)
+            {
+                MessageBox.Show("Правильно");
+                b = b + 1;
+            }
+            else MessageBox.Show("Неправильно");
+
+            p3.Visibility = Visibility.Collapsed;
+           
+        }
+    }
+}
